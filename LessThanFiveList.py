ca7cf8aa008b721718d8feb8e8f7724690683925
BraydenRoyston <43502771+BraydenRoyston@users.noreply.github.com>
2018-12-20 14:21:36 -0500

    Update LessThanFiveList.py

--- LessThanFiveList.py
+++ LessThanFiveList.py
@@ -1,3 +1,7 @@
+# This program takes an input of a list, and then take an input of a number.
+# The program then outputs any numbers in the initial list that are less than the second input.
+
+
 list1 = [int(x) for x in input("Enter the list, and be sure to separate different arguments by a space:").split()]
 list2 = []
 y = int(input("Enter the integer you want the results to be less than:"))
