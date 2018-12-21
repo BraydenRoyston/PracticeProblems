148ea425afb6554229af1700e9ee439e2bc5140e
BraydenRoyston <43502771+BraydenRoyston@users.noreply.github.com>
2018-12-20 19:40:49 -0500

    Add files via upload

---
+++ Turning 100.py
@@ -0,0 +1,7 @@
+print("Hello! Please enter your name!")
+name = input()
+
+print(name + ", please what year you were born in!")
+year = int(input())
+
+print(name + ", you will turn 100 in the year " + str(year + 100) + ".")
