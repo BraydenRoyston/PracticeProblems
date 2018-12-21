65381eaf8181fd40fb202936b7ba96d77d442f6d
BraydenRoyston <43502771+BraydenRoyston@users.noreply.github.com>
2018-12-20 19:41:56 -0500

    Update Turning 100.py

--- Turning 100.py
+++ Turning 100.py
@@ -1,3 +1,6 @@
+# this program takes an input of a name and the year the use was born in, then displays a personalized message detailing
+# what year the use will turn 100 in.
+
 print("Hello! Please enter your name!")
 name = input()
 
